d2ab0df9e849de3afcd2e90d3b26640f10677975
Jhonatan <95220392+Naylor-C@users.noreply.github.com>
2025-04-27 09:51:31 -0300

    Update config_frame.h

--- main/include/window/config_frame.h
+++ main/include/window/config_frame.h
@@ -1,1 +1,14 @@
-_
+#include <gtkmm.h>
+
+class Frame : public Gtk::Window
+{
+public:
+  Frame();
+  virtual ~Frame();
+
+protected:
+  //Signal handlers:
+  void on_button_quit();
+  void on_treeview_row_activated(const Gtk::TreeModel::Path& path, Gtk::TreeViewColumn* column);
+};
+
