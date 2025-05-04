d3c7ded1f44b87c16ae2ad5e0ff84ea98cf38327
Naylor-C <jhonatanproject1997@gmail.com>
2025-05-04 20:53:18 -0300

    linux primary

---
+++ main/src/app.cpp
@@ -0,0 +1,8 @@
+#include "Window/frame.h"
+
+int main(int argc, char* argv[])
+{
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+  return app->make_window_and_run<Frame>(argc, argv);
+}
